cdf56adaadabcf9fb141864ede629b8365afe1c3
LEXAMETA <lexagreeter@gmail.com>
2025-06-23 11:50:08 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config')
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts.push('sql')
-config.resolver.assetExts.push('gguf')
-
-module.exports = config
-
+module.exports = {
+  ...config,
+  transformer: {
+    ...config.transformer,
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+  resolver: {
+    ...config.resolver,
+    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'cjs', 'sql'], // Keep .sql for mobile
+    assetExts: ['png', 'jpg', 'jpeg', 'ttf', 'woff', 'woff2', 'otf', 'svg', 'gguf'], // Keep .gguf for mobile
+  },
+};
